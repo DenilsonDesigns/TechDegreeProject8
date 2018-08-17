ecd769b05e3bf3eb3692e4a6025f5443f7432cea
DenilsonDesigns <daniellanguilller@gmail.com>
2018-08-17 15:14:15 +1000

    gulp build added

--- gulpfile.js
+++ gulpfile.js
@@ -7,25 +7,29 @@ const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const maps = require("gulp-sourcemaps");
 const del = require("del");
-const cleanCSS = require('gulp-clean-css');
+const cleanCSS = require("gulp-clean-css");
+const imagemin = require("gulp-imagemin");
 
+// TASK1************************
 gulp.task("concatScripts", () => {
   return gulp
     .src(["js/circle/autogrow.js", "js/circle/circle.js"])
     .pipe(maps.init())
-    .pipe(concat("global.js"))
+    .pipe(concat("all.js"))
     .pipe(maps.write("./"))
     .pipe(gulp.dest("js"));
 });
 
-gulp.task("minifyScripts", ["concatScripts"], () => {
+gulp.task("script", ["concatScripts"], () => {
   return gulp
-    .src("js/global.js")
+    .src("js/all.js")
     .pipe(uglify())
-    .pipe(rename("global.min.js"))
-    .pipe(gulp.dest("js"));
+    .pipe(rename("all.min.js"))
+    .pipe(gulp.dest("dist/scripts"));
 });
+//********************************TASK 1 */
 
+//TASK 2 ******************************
 gulp.task("compileSass", () => {
   return gulp
     .src("sass/global.scss")
@@ -35,38 +39,51 @@ gulp.task("compileSass", () => {
     .pipe(gulp.dest("css"));
 });
 
-gulp.task("minifyCss", ["compileSass"], () => {
-   return gulp
-     .src("css/global.css")
-     .pipe(cleanCSS({compatibility: 'ie8'}))
-     .pipe(rename("global.min.css"))
-     .pipe(gulp.dest("css"));
- });
+gulp.task("styles", ["compileSass"], () => {
+  return gulp
+    .src("css/global.css")
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(rename("all.min.css"))
+    .pipe(gulp.dest("dist/styles"));
+});
+//*************************TASK 2 */
+
+//TASK 5 **************************
+gulp.task("images", () => {
+  gulp
+    .src("images/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/content"));
+});
+//*********************TASK 5 */
 
 gulp.task("watchFiles", () => {
   gulp.watch(["sass/**/*.scss", "sass/**/*.sass"], ["compileSass"]);
   gulp.watch("js/global.js", ["concatScripts"]);
 });
 
+//TASK 6 *************************************/
 gulp.task("clean", () => {
-  del(["dist", "js/global.js", "css", "js/global.min.js", "js/global.js.map"]);
+  del(["dist", "js/all.js", "css", "js/all.min.js", "js/all.js.map"]);
 });
-
-gulp.task("build", ["minifyScripts", "minifyCss"], () => {
-  return gulp
-    .src(
-      [
-        "css/global.min.css",
-        "js/global.min.js",
-        "index.html",
-        "images/**",
-        "icons/**",
-        "sass/**"
-      ],
-      { base: "./" }
-    )
-    .pipe(gulp.dest("dist"));
-});
+//**********TASK 6 **************************/
+// gulp.series('clean', gulp.parallel('scripts', 'styles'),
+gulp.task("build", ['clean', 'styles', 'script', 'images'], () => {
+    return gulp
+      .src(
+        [
+          "css/global.min.css",
+          "js/global.min.js",
+          "index.html",
+          "images/**",
+          "icons/**",
+          "sass/**"
+        ],
+        { base: "./" }
+      )
+      .pipe(gulp.dest("dist"));
+  }
+);
 
 gulp.task("serve", ["watchFiles"]);
 
