5f4087aab21fc2b786ddfa1974dd246af7ff7024
DenilsonDesigns <daniellanguilller@gmail.com>
2018-08-17 14:08:12 +1000

    finished basic gulp build-out

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,10 @@ const uglify = require("gulp-uglify");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const maps = require("gulp-sourcemaps");
+const del = require("del");
 
 gulp.task("concatScripts", () => {
-  gulp
+  return gulp
     .src(["js/circle/autogrow.js", "js/circle/circle.js"])
     .pipe(maps.init())
     .pipe(concat("global.js"))
@@ -16,8 +17,8 @@ gulp.task("concatScripts", () => {
     .pipe(gulp.dest("js"));
 });
 
-gulp.task("minifyScripts", () => {
-  gulp
+gulp.task("minifyScripts", ["concatScripts"], () => {
+  return gulp
     .src("js/global.js")
     .pipe(uglify())
     .pipe(rename("global.min.js"))
@@ -25,7 +26,7 @@ gulp.task("minifyScripts", () => {
 });
 
 gulp.task("compileSass", () => {
-  gulp
+  return gulp
     .src("sass/global.scss")
     .pipe(maps.init())
     .pipe(sass())
@@ -33,6 +34,33 @@ gulp.task("compileSass", () => {
     .pipe(gulp.dest("css"));
 });
 
-gulp.task("default", ["hello"], () => {
-  console.log("Default finished");
+gulp.task("watchFiles", () => {
+  gulp.watch(["sass/**/*.scss", "sass/**/*.sass"], ["compileSass"]);
+  gulp.watch("js/global.js", ["concatScripts"]);
+});
+
+gulp.task("clean", () => {
+  del(["dist", "js/global.js", "css", "js/global.min.js", "js/global.js.map"]);
+});
+
+gulp.task("build", ["minifyScripts", "compileSass"], () => {
+  return gulp
+    .src(
+      [
+        "css/global.css",
+        "js/global.min.js",
+        "index.html",
+        "images/**",
+        "icons/**",
+        "sass/**"
+      ],
+      { base: "./" }
+    )
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("serve", ["watchFiles"]);
+
+gulp.task("default", ["clean"], () => {
+  gulp.start("build");
 });
