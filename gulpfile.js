6bc94974acb616e7cbc5dc09a9a9a6347c3d3866
DenilsonDesigns <daniellanguilller@gmail.com>
2018-08-20 10:10:11 +1000

    fixed directory listing

--- gulpfile.js
+++ gulpfile.js
@@ -93,7 +93,7 @@ gulp.task('webserver', function() {
   gulp.src('./')
     .pipe(webserver({
       livereload: true,
-      directoryListing: true,
+      directoryListing: false,
       open: true,
       port: 3000,
     }));
